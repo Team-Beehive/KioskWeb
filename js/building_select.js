29306711df0eb2d56b297117072233b835721414
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-03-02 15:29:49 -0800

    Firestore is working

--- js/building_select.js
+++ js/building_select.js
@@ -1,23 +1,18 @@
 // with npm
 
-//Import the functions you need from the SDKs you need
-/*
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
-import { collection, doc, setDoc } from "firebase/firestore";
-*/
+//Import the functions you need from the SDKs yo
 
 // with Commonjs syntax (if using Node)
-const firebase = require("firebase/app");
-require("firebase/firestore");
+// const firebase = require("firebase/app");
+// require("firebase/firestore");
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig =
-{
+firebase.initializeApp({
+
     apiKey: "AIzaSyCwReoKDSMZgqVD1BvOb5aUQi3QJALE7hc",
     authDomain: "oit-kiosk.firebaseapp.com",
     projectId: "oit-kiosk",
@@ -25,15 +20,20 @@ const firebaseConfig =
     messagingSenderId: "622074473491",
     appId: "1:622074473491:web:f3fb2717a8577b8ff963e6",
     measurementId: "G-BYNMKM80XC"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Get a reference to the database service
-const database = getDatabase(app);
+
+
+var db = firebase.firestore();
+
+db.collection("pages").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
 
 // the hello world program
 console.log('Hello World');
 
-const querySnapshot = await getDocs(collections(db, "pages").getDocs("Map").collection("Buildings"));
-querySnapshot.forEach(doc => { console.log(`${doc.id} => ${doc.data()}`) });
+// const querySnapshot = await db.getDocs(db.collections(database, "pages").getDocs("Map").collection("Buildings"));
+// querySnapshot.forEach(doc => { console.log(`${doc.id} => ${doc.data()}`) });
