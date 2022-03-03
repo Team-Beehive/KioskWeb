eac3c8d08231c0fa41a9e887b3d6cd7e55643f38
skittlemassacare <zombiedude723@gmail.com>
2022-03-03 10:16:47 -0800

    connected to firestore db working on  making functions to access data -Works -Does nothing besides connect to firestore db and print hello world

--- js/building_select.js
+++ js/building_select.js
@@ -1,15 +1,7 @@
-// with Commonjs syntax (if using Node)
-const firebase = require("firebase/app");
-require("firebase/firestore");
-
-// TODO: Add SDKs for Firebase products that you want to use
-//
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig =
-{
+firebase.initializeApp({
+
     apiKey: "AIzaSyCwReoKDSMZgqVD1BvOb5aUQi3QJALE7hc",
     authDomain: "oit-kiosk.firebaseapp.com",
     projectId: "oit-kiosk",
@@ -17,18 +9,17 @@ const firebaseConfig =
     messagingSenderId: "622074473491",
     appId: "1:622074473491:web:f3fb2717a8577b8ff963e6",
     measurementId: "G-BYNMKM80XC"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Get a reference to the database service
-const database = getDatabase(app);
+
+
+var db = firebase.firestore();
+
+db.collection("pages").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
 
 // the hello world program
 console.log('Hello World');
-
-async function GetDocs()
-{
-	const querySnapshot = await getDocs(collections(db, "pages").getDocs("Map").collection("Buildings"));
-	querySnapshot.forEach(doc => { console.log(`${doc.id} => ${doc.data()}`) });
-}
