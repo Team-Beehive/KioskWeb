dfacc3c94d4beb928b5118864095b7450bc35eb6
skittlemassacare <zombiedude723@gmail.com>
2022-03-08 09:21:37 -0800

    Merged 'WebKioskdb' into 'main'

--- js/building_select.js
+++ js/building_select.js
@@ -1,14 +1,10 @@
-// with Commonjs syntax (if using Node)
-const firebase = require("firebase/app");
-require("firebase/firestore");
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+//https://firebase.google.com/docs/reference/js/v8
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig =
-{
+firebase.initializeApp({
+
     apiKey: "AIzaSyCwReoKDSMZgqVD1BvOb5aUQi3QJALE7hc",
     authDomain: "oit-kiosk.firebaseapp.com",
     projectId: "oit-kiosk",
@@ -16,18 +12,32 @@ const firebaseConfig =
     messagingSenderId: "622074473491",
     appId: "1:622074473491:web:f3fb2717a8577b8ff963e6",
     measurementId: "G-BYNMKM80XC"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Get a reference to the database service
-const database = getDatabase(app);
-
-// the hello world program
-console.log('Hello World');
-
-async function GetDocs()
+function saveData(id, about, campuses, type)
 {
-	const querySnapshot = await getDocs(collections(db, "pages").getDocs("Map").collection("Buildings"));
-	querySnapshot.forEach(doc => { console.log(`${doc.id} => ${doc.data()}`) });
+
 }
+
+
+
+var db = firebase.firestore();
+//gets doccument snapshot for every doc in collection
+db.collection("pages").doc("Majors").collection("Degrees").get().then((querySnapshot) => {
+    let major_data = new Map();
+    querySnapshot.forEach((doc) => {
+
+        //Prints of id of each doc in collection
+        major_data.set("id", doc.id);
+        //Prints the about section of the doc
+        major_data.set("about", doc.get("about"));
+        //Prints the campuses section of the doc
+        major_data.set("campuses", doc.get("campuses"));
+        //Prints the type section of the doc
+        major_data.set("type", doc.get("type"));
+        console.log(major_data.get("id"), major_data.get("about"), major_data.get("campuses"), major_data.get("type"));
+
+    });
+});
+
+console.log('Hello World');
