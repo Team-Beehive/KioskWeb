c6a88ab97b71632a36addb1096ac4112fd567ab6
skittlemassacare <zombiedude723@gmail.com>
2022-03-09 23:17:35 -0800

    Saves locally on device -Works

--- js/building_select.js
+++ js/building_select.js
@@ -14,6 +14,22 @@ firebase.initializeApp({
     measurementId: "G-BYNMKM80XC"
 });
 
+//stack code
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+
+//stack code
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+//Function that take a PageData and prints out its data
+function PrintData(page_data)
+{
+    console.log(page_data.id, page_data.about, page_data.campuses, page_data.type)
+}
+
 //Class to hold each majors data from the database
 class PageData
 {
@@ -26,26 +42,26 @@ class PageData
     }
 }
 
-let major_data = new Array();
+//Database obj
 var db = firebase.firestore();
 
 //gets doccument snapshot for every doc in collection
 db.collection("pages").doc("Majors").collection("Degrees").get().then((querySnapshot) => {
 
+    //clears local stoarage before writing to it, dont know how it cleans up
+    localStorage.clear();
+
     //Gets the data and saves it into the PageData class
     querySnapshot.forEach((doc) => {
         temp_data = new PageData(doc.id, doc.get("about"), doc.get("campuses"), doc.get("type"));
-        major_data.push(temp_data);
+        localStorage.setObj(doc.id, temp_data)
     });
 
-    //Prints all the data to the console
-    major_data.forEach(PrintData);
-});
+    //test to see if it gets everything
+    querySnapshot.forEach((doc) => {
+        PrintData(localStorage.getObj(doc.id));
+    });
 
-//Function that take a PageData and prints out its data
-function PrintData(page_data)
-{
-    console.log(page_data.id, page_data.about, page_data.campuses, page_data.type)
-}
+});
 
 console.log('Hello World');
