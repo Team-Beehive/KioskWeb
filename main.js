433764341e71a65d7c11a613dc088684bd60869c
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-04-06 18:24:17 -0700

    Added some extra stuff as well as some checking for packages

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const path = require("path");
 const models = require("./models");
 
-const {execSync} = require("child_process");
+const { execSync, exec } = require("child_process");
 
 
 const app = initializeApp({
@@ -20,6 +20,29 @@ const app = initializeApp({
 
 const db = getFirestore(app);
 
+let termux_api = false;
+let adb = false;
+
+try {
+    execSync("dpkg -l | grep android-tools", { stdio: ["ignore", "ignore", "ignore"] });
+    adb = true;
+}
+catch
+{
+    console.error("Package android-tools is not installed. Locking will not work.");
+}
+
+try {
+    execSync("dpkg -l | grep termux-api", { stdio: ["ignore", "ignore", "ignore"] });
+    termux_api = true;
+}
+catch
+{
+    console.error("Application Termux:API is not installed. Some non-key features will not work.");
+}
+
+console.log();
+
 express()
     .use(express.static(path.join(__dirname, "public")))
     .use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
@@ -30,14 +53,11 @@ express()
 
     .get("/", (req, res) => res.render("pages/links"))
     // .get("/building_select", (req, res) => res.render("pages/building_select"))
-    .get("/major_select", (req, res) => 
-    {
-        getDoc(doc(db, "pages", "Majors")).then((snapshot, options) =>
-        {
+    .get("/major_select", (req, res) => {
+        getDoc(doc(db, "pages", "Majors")).then((snapshot, options) => {
             let data = snapshot.data(options);
-            if (data != undefined)
-            {
-                res.render("pages/major_select", {categories:  data["Categories"] });
+            if (data != undefined) {
+                res.render("pages/major_select", { categories: data["Categories"] });
             }
             else
                 res.render("pages/404");
@@ -53,50 +73,50 @@ express()
         if (major != undefined)
             getDoc(doc(db, "pages", "Majors", "Degrees", major)).then((snapshot, options) => {
                 let data = snapshot.data(options);
-                if (data != undefined)
-                {
+                if (data != undefined) {
                     let temp_data = new models.MajorPageData(snapshot.id, data["about"], data["campuses"], data["type"]);
                     res.render("pages/major", { major: temp_data });
                 }
-                else
-                {
+                else {
                     res.render("pages/404");
                 }
             });
         else
             res.render("pages/404");
     })
-    .get("/hide_nav", (req, res) => 
-    {
-        try
-        {
+    .get("/hide_nav", (req, res) => {
+        if (adb) {
             execSync("adb connect localhost:55555");
             execSync("adb shell wm overscan 0,-100,0,-100");
             execSync("adb disconnect");
 
-            res.render("pages/links", { message: "successfully hid navigation bars!" });
+            if (termux_api)
+                exec("termux-toast 'Successfully locked application!'");
+            else
+                console.log("Successfully locked application!");
+
+            res.redirect("/links");
         }
-        catch
-        {
-            res.render("pages/links", {message: "problem occurred when trying to hide navigation bar."});
-        }
-        
+        else
+            res.redirect("/links");
     })
-    .get("/show_nav", (req, res) =>
-    {
-        try {
+    .get("/show_nav", (req, res) => {
+        if (adb) {
             execSync("adb connect localhost:55555");
             execSync("adb shell wm overscan 0,0,0,0");
             execSync("adb disconnect");
 
-            res.render("pages/links", { message: "successfully showed navigation bars!" });
+            if (termux_api)
+                exec("termux-toast 'Successfully unlocked application!'");
+            else
+                console.log("Successfully unlocked application!");
+
+            res.redirect("/links");
         }
-        catch
-        {
-            res.render("pages/links", { message: "problem occurred when trying to show navigation bar." });
-        }
+        else
+            res.redirect("/links");
     })
     .get("/old_building_select", (req, res) => res.render("pages/old_building_select"))
     .get("*", (req, res) => res.render("pages/404")) // 404 Handler
     .disable("x-powered-by") // Prevents end users from knowing that the server is express
-    .listen(PORT, () => console.log(`Started server on http://localhost:${ PORT }`));
+    .listen(PORT, () => console.log(`Started server on http://localhost:${PORT}`));
