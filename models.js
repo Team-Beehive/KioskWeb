1b170b0a661d02ddb5fd521d2a9a482ee21c1876
skittlemassacare <zombiedude723@gmail.com>
2022-04-19 15:58:39 -0700

    I added my code to make it read from the json after reading it in from the online database. For some reason when you go to major select it throws an error saying the header is already set. Other then that it doesn't crash and you can access major pages and categories although not through categories.

--- models.js
+++ models.js
@@ -1,28 +1,145 @@
-module.exports.MajorPageData = class MajorPageData {
-    constructor(id, about, campuses, type) {
+//Require for file manipulation on system
+var fs = require("fs");
+
+//holds the indiual page data for each major
+module.exports.PageData = class PageData {
+    constructor(id, about, campuses, type, key) {
         this.id = id;
         this.about = about;
         this.campuses = campuses;
         this.type = type;
+        this.keyCategory = key;
     }
 };
 
+//Class that holds all the page data in pages array , has a get set for pages. Takes nothing.
+module.exports.Pages = class Pages{
+    pages = [];
+
+    GetPages(){
+        return this.pages;
+    }
+    
+    AddPageData(PageData){
+        this.pages.push(PageData);
+    }
+}
+
+//holds a list of PageData
+module.exports.Category = class Category
+{
+    title;
+    pageData = [];
+    
+    constructor(categoryTitle){
+        this.title = categoryTitle;
+    }
+    
+    GetPageData(){
+        return this.pageData;
+    }
+    
+    AddPageData(PageData){
+        this.pageData.push(PageData);
+    }
+}
+
+
+//Holds a list of catagories
 module.exports.CollectionData = class CollectionData
 {
-    cData = [];
-    constructor(datetime)
-    {
-        this.datetime = datetime;
+    datetime;
+    categories = [];
+    categoryData = [];
+
+    constructor(){
+        this.datetime = Date();
     }
 
-    AddData(majorData)
-    {
-        this.cData.push(majorData);
+    GetCategories(){
+        if(this.categories != undefined){
+            return this.categories;
+        }
     }
 
-    PrintDate()
-    {
+    AddCategories(majorData){
+        if(majorData != undefined){
+            this.categories.push(majorData);
+        }
+    }
+
+    AddCategoryData(majorData){
+        if(majorData != undefined){
+            this.categoryData.push(majorData);
+        }
+    }
+
+    AddPageData(PageData){
+        this.pageData.push(PageData);
+    }
+
+    PrintDate(){
         console.log(this.datetime);
+    }
+    
+    GetDataJson()
+    {
+        var data = fs.readFileSync("categories.json"), myObj;
+
+        try {
+            myObj = JSON.parse(data);
+            if(myObj != undefined){
+                return myObj;
+            }
+        }
+        catch (err) {
+            console.log("There has been an error parsing data from data.JSON");
+            console.log(err);
+        }
+    }
+
+    SaveDataJson(myObj){
+        var jdata = JSON.stringify(myObj);
+        
+        fs.writeFileSync("categories.json", jdata, function (err) {
+            if (err) {
+                console.log("There has been an error saving your configuration data.");
+                console.log(err.message);
+            }
+            else { 
+                console.log("Configuration saved successfully.");
+            }
+        }); 
+    }
+
+    SavePagesJson(myObj){
+        var jdata = JSON.stringify(myObj);
+    
+        fs.writeFileSync("./pages.json", jdata, {flag: 'w+'}, function (err) {
+            if (err) {
+                console.log("There has been an error saving your pages.");
+                console.log(err.message);
+            }
+            else { 
+                console.log("Pages saved successfully.");
+            }
+        }); 
+    }
+
+    GetPagesJson()
+    {
+        var data = fs.readFileSync("./pages.json"), myObj;
+
+        try {
+            myObj = JSON.parse(data);
+            if(myObj != undefined){
+                return myObj;
+            }
+        }
+        catch (err) {
+            console.log("There has been an error parsing data from pages.JSON");
+            console.log(err);
+        }
     }
 };
 
