4a27c6be15f62509b0b1b6db35ddbe7a45b5887a
TuckerN-Code <nultytucker@gmail.com>
2022-04-22 13:15:36 -0700

    Revert "I added my code to make it read from the json after reading it in from the online database. For some reason when you go to major select it throws an error saying the header is already set. Other then that it doesn't crash and you can access major pages and categories although not through categories."
    This reverts commit 1b170b0a661d02ddb5fd521d2a9a482ee21c1876.

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const PORT = process.env.PORT || 8080;
 const path = require("path");
 const models = require("./models");
 
+
+const app = initializeApp({
+    apiKey: "AIzaSyCwReoKDSMZgqVD1BvOb5aUQi3QJALE7hc",
+    authDomain: "oit-kiosk.firebaseapp.com",
+    projectId: "oit-kiosk",
+    storageBucket: "oit-kiosk.appspot.com",
+    messagingSenderId: "622074473491",
+    appId: "1:622074473491:web:f3fb2717a8577b8ff963e6",
+    measurementId: "G-BYNMKM80XC"
+});
+
+const db = getFirestore(app);
+
+
 express()
     .use(express.static(path.join(__dirname, "public")))
     .use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
@@ -19,32 +33,14 @@ express()
         res.render("pages/building_select", {buildings: models.buildings});
     })
     .get("/major_select", (req, res) => {
-        //Start: get all data
-        collectionData = new models.CollectionData();
-        //gets the array of categories from json
-        classCollection = collectionData.GetDataJson();
-        //gets all the data for each page from json
-        classPages = collectionData.GetPagesJson();
-        
-        classCollection.categories.forEach(category =>{
-            tempCategory = new models.Category(category);
-     
-            classPages.pages.forEach(page =>{
-                if(page.keyCategory == category){
-                    tempCategory.AddPageData(page);
-                }
-            })
-     
-            collectionData.AddCategoryData(tempCategory);
-        })//End: get all data   
-
-        try{
-            res.render("pages/major_select", { categories: collectionData.categoryData});
-        }
-        catch(err){
-            console.log(err);
-            res.render("pages/404");
-        }
+        getDoc(doc(db, "pages", "Majors")).then((snapshot, options) => {
+            let data = snapshot.data(options);
+            if (data != undefined) {
+                res.render("pages/major_select", { categories: data["Categories"], bad_chars: /[ &/]/g });
+            }
+            else
+                res.render("pages/404");
+        });
     })
     .get("/home_page", (req, res) => res.render("pages/home_page"))
     .get("/building", (req, res) => {
@@ -53,71 +49,21 @@ express()
     })
     .get("/major", (req, res) => {
         let major = req.query.page;
-        
-            collectionData = new models.CollectionData();
-            classPages = collectionData.GetPagesJson();
-            classPages.pages.forEach(page =>{
-                if(page.id = major){
-                    tempData = page;
+        if (major != undefined)
+            getDoc(doc(db, "pages", "Majors", "Degrees", major)).then((snapshot, options) => {
+                let data = snapshot.data(options);
+                if (data != undefined) {
+                    let temp_data = new models.MajorPageData(snapshot.id, data["about"], data["campuses"], data["type"]);
+                    res.render("pages/major", { major: temp_data });
                 }
-            })
-            res.render("pages/major", { major: tempData });
+                else {
+                    res.render("pages/404");
+                }
+            });
+        else
             res.render("pages/404");
     })
     .get("/old_building_select", (req, res) => res.render("pages/old_building_select"))
     .get("*", (req, res) => res.render("pages/404")) // 404 Handler
     .disable("x-powered-by") // Prevents end users from knowing that the server is express
-    .listen(PORT, () =>{
-        //tries to query all data and saves it into two jsons
-            const app = initializeApp({
-                apiKey: "AIzaSyCwReoKDSMZgqVD1BvOb5aUQi3QJALE7hc",
-                authDomain: "oit-kiosk.firebaseapp.com",
-                projectId: "oit-kiosk",
-                storageBucket: "oit-kiosk.appspot.com",
-                messagingSenderId: "622074473491",
-                appId: "1:622074473491:web:f3fb2717a8577b8ff963e6",
-                measurementId: "G-BYNMKM80XC"
-            });
-            
-            const db = getFirestore(app);
-
-            getDoc(doc(db, "pages", "Majors")).then((snapshot, options) =>
-            {
-                if(snapshot != null){
-                    let collectionData = new models.CollectionData();
-                    let pages = new models.Pages();
-                    let data = snapshot.data(options);
-                    count = 0;
-                    
-                    data["Categories"].forEach(category => {
-                        collectionData.AddCategories(category["categoryTitle"]);
-    
-                        category["relatedDegrees"].forEach(degreeRef => { 
-    
-                            getDoc(doc(db, "pages", "Majors", "Degrees", degreeRef.id)).then((snapshot, options) => {
-                                let data = snapshot.data(options);
-    
-                                if (data != undefined) {
-                                    //will read twice without if
-                                    if(count % 2 == 0){
-                                        tempPage = new models.PageData(snapshot.id, data["about"], data["campuses"], data["type"], category["categoryTitle"]);
-                                        pages.AddPageData(tempPage);
-                                        //ooverites each time so the last overitw will write all data, saves all the data for the pages in the pages class in an array
-                                        collectionData.SavePagesJson(pages);
-                                    } 
-                                    count++;
-                                }
-                            });
-                        });
-                    
-                    });
-                    collectionData.SaveDataJson(collectionData);
-                }
-                else{
-                    console.log("Failed to read data from online database.");
-                }
-                //saves all the category data here
-            });
-
-        console.log(`Started server on http://localhost:${ PORT }`
-    )})       
+    .listen(PORT, () => console.log(`Started server on http://localhost:${ PORT }`));
--- models.js
+++ models.js
@@ -1,145 +1,28 @@
-//Require for file manipulation on system
-var fs = require("fs");
-
-//holds the indiual page data for each major
-module.exports.PageData = class PageData {
-    constructor(id, about, campuses, type, key) {
+module.exports.MajorPageData = class MajorPageData {
+    constructor(id, about, campuses, type) {
         this.id = id;
         this.about = about;
         this.campuses = campuses;
         this.type = type;
-        this.keyCategory = key;
     }
 };
 
-//Class that holds all the page data in pages array , has a get set for pages. Takes nothing.
-module.exports.Pages = class Pages{
-    pages = [];
-
-    GetPages(){
-        return this.pages;
-    }
-    
-    AddPageData(PageData){
-        this.pages.push(PageData);
-    }
-}
-
-//holds a list of PageData
-module.exports.Category = class Category
-{
-    title;
-    pageData = [];
-    
-    constructor(categoryTitle){
-        this.title = categoryTitle;
-    }
-    
-    GetPageData(){
-        return this.pageData;
-    }
-    
-    AddPageData(PageData){
-        this.pageData.push(PageData);
-    }
-}
-
-
-//Holds a list of catagories
 module.exports.CollectionData = class CollectionData
 {
-    datetime;
-    categories = [];
-    categoryData = [];
-
-    constructor(){
-        this.datetime = Date();
+    cData = [];
+    constructor(datetime)
+    {
+        this.datetime = datetime;
     }
 
-    GetCategories(){
-        if(this.categories != undefined){
-            return this.categories;
-        }
+    AddData(majorData)
+    {
+        this.cData.push(majorData);
     }
 
-    AddCategories(majorData){
-        if(majorData != undefined){
-            this.categories.push(majorData);
-        }
-    }
-
-    AddCategoryData(majorData){
-        if(majorData != undefined){
-            this.categoryData.push(majorData);
-        }
-    }
-
-    AddPageData(PageData){
-        this.pageData.push(PageData);
-    }
-
-    PrintDate(){
+    PrintDate()
+    {
         console.log(this.datetime);
-    }
-    
-    GetDataJson()
-    {
-        var data = fs.readFileSync("categories.json"), myObj;
-
-        try {
-            myObj = JSON.parse(data);
-            if(myObj != undefined){
-                return myObj;
-            }
-        }
-        catch (err) {
-            console.log("There has been an error parsing data from data.JSON");
-            console.log(err);
-        }
-    }
-
-    SaveDataJson(myObj){
-        var jdata = JSON.stringify(myObj);
-        
-        fs.writeFileSync("categories.json", jdata, function (err) {
-            if (err) {
-                console.log("There has been an error saving your configuration data.");
-                console.log(err.message);
-            }
-            else { 
-                console.log("Configuration saved successfully.");
-            }
-        }); 
-    }
-
-    SavePagesJson(myObj){
-        var jdata = JSON.stringify(myObj);
-    
-        fs.writeFileSync("./pages.json", jdata, {flag: 'w+'}, function (err) {
-            if (err) {
-                console.log("There has been an error saving your pages.");
-                console.log(err.message);
-            }
-            else { 
-                console.log("Pages saved successfully.");
-            }
-        }); 
-    }
-
-    GetPagesJson()
-    {
-        var data = fs.readFileSync("./pages.json"), myObj;
-
-        try {
-            myObj = JSON.parse(data);
-            if(myObj != undefined){
-                return myObj;
-            }
-        }
-        catch (err) {
-            console.log("There has been an error parsing data from pages.JSON");
-            console.log(err);
-        }
     }
 };
 
