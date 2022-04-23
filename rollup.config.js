9d47229646c3b5a646f46ac9e963b17325993a84
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-04-23 15:30:08 -0700

    Updated scripts

--- rollup.config.js
+++ rollup.config.js
@@ -10,14 +10,5 @@ export default
             name: "bootstrap"
         },
         plugins: [nodeResolve()]
-    },
-    {
-        input: "./imports/firebase_import.js",
-        output: {
-            file: "./build/firebase.bundle.js",
-            format: "es",
-            name: "firebase"
-        },
-        plugins: [nodeResolve()]
     }
 ];
