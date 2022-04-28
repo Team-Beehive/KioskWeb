6676dc26d789dcc43a4bb18bf084e091eaab639b
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-04-27 21:29:06 -0700

    Removed console.logs, removed firebase_import

--- imports/firebase_import.js
+++
@@ -1,4 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-
-export { initializeApp, getFirestore, collection, getDocs };
