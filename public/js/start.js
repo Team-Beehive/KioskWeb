f0e2a21b8789c0fec0365541fdbb9f5c0fbeb4c9
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-05-12 17:00:34 -0700

    Even better scroll handling

--- public/js/start.js
+++ public/js/start.js
@@ -4,28 +4,34 @@ function inDiv(string)
 }
 
 $(() => {
-    function animateThis(targetElement, first, width, speed) {
-        $(targetElement).scrollLeft(width + $(first).width() - $(window).width());
+    // Adapted from https://stackoverflow.com/a/39339457
+    function animateThis(targetElement, first, speed) {
         var scrollWidth = $(targetElement).get(0).scrollWidth;
         var clientWidth = $(targetElement).get(0).clientWidth;
+
+        $(targetElement).scrollLeft((scrollWidth - $(first).width()) / 2 + $(first).width() - $(window).width());
+
         $(targetElement).animate({ scrollLeft: scrollWidth - clientWidth },
             {
                 duration: speed,
                 easing: "linear",
                 complete: () => {
-                    animateThis(targetElement, first, width, speed);
+                    animateThis(targetElement, first, speed);
                 }
             });
     }
+
     let first = $(".scroll").children()[0];
-    let width = document.getElementById("scroll").scrollWidth;
+
     let str = "";
     let len = $(".scroll").children().length;
     for (let i = 0; i < len; i++) {
         const element = $(".scroll").children()[i];
         str += inDiv($(element).html());
     }
+
     $(".scroll").prepend(str);
     $(".scroll").append(inDiv($(first).html()));
-    animateThis($(".scroll"), first, width, 25000);
+    
+    animateThis($(".scroll"), first, 25000);
 });
