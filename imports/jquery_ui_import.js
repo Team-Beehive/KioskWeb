0a4de6fa3b5dc98c1d14b132c46b3416d77f6531
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-05-19 13:06:03 -0700

    Finishing up after work

--- imports/jquery_ui_import.js
+++ imports/jquery_ui_import.js
@@ -1,7 +1,9 @@
-import "jquery";
+import * as jQuery from "jquery";
 import "jquery-ui";
+import "jquery-ui/ui/core";
+import "jquery-ui/ui/widget";
 import "jquery-ui/ui/widgets/draggable";
 import "jquery-ui/ui/widgets/droppable";
-// import "jquery-ui/ui/widgets/mouse";
+import "jquery-ui/ui/widgets/mouse";
 
-// export { jquery };
+export { jQuery };
--- public/js/building_secret.js
+++ public/js/building_secret.js
@@ -1,4 +1,5 @@
 $(() => {
+    console.log($.ui === undefined);
     $("#secret").draggable();
     $("#secret-drop").droppable(
         {
