09acdd2f9bb3879b0d751ff34089b9be6370f9ad
Trevor W <34048315+trevor34@users.noreply.github.com>
2022-05-23 15:42:13 -0700

    Giving credit, also clearing out some edge cases

--- public/js/start.js
+++ public/js/start.js
@@ -1,18 +1,14 @@
 $(() => {
-    // While this bit may seem strange, it's to make sure that when the images are scrolling 
-    // and it jumps back to the first image, there's a slim-to-none chance that there's any visual 
-    // disturbance. This takes all the children of the scroll and prepends them to the scroll div. 
-    // Firstly, it gets all of the images in the scroll div and puts them in the string.
-    // Next, it grabs the scroll width and client width, and while the scroll width is less 
-    // than twice the client width, it prepends the scroll contents into the front of the scroll.
-    // Finally, it appends the first image of the div to the last one for an infinite scroll effect.
-    let first = $(".photobanner").children().first();
-    let str = "";
+    // Appends the same images to the end of the photobanner for an infinite scrolling effect.
+    // Repeats until it's at least twice the size of the screen width just for good measure.
+    // Makes it so that once it hops back to the start, it won't cause any strange effects.
 
-    let len = $(".photobanner").children().length;
-    if (len > 0)
+    let imageAmount = $(".photobanner").children().length;
+    if (imageAmount > 0)
     {
-        for (let i = 0; i < len; i++) {
+        let str = "";
+
+        for (let i = 0; i < imageAmount; i++) {
             const element = $(".photobanner").children()[i];
             str += element.outerHTML;
         }
@@ -20,21 +16,30 @@ $(() => {
         var bannerWidth = $($(".photobanner").get(0)).width();
         var documentWidth = $(document).width();
 
+        var copyAmount = 1; // Starts with the one original copy
         do {
-            $(".photobanner").prepend(str);
+            $(".photobanner").append(str);
 
             bannerWidth = $($(".photobanner").get(0)).width();
             documentWidth = $(document).width();
-        } while (bannerWidth <= documentWidth * 1.5);
+            copyAmount++;
+        } while (bannerWidth <= documentWidth * 2);
 
-
-        $(".photobanner").append(first.outerHTML);
-
-        documentWidth = $(document).width();
+        // Depending on the amount of copies, we will have to change where at in the rotation
+        // it goes back at. 
+        // e.g., at 2 copies it'll at go back at 50%, at 3 copies, 33.33%, 4, 25%, etc.
+        document.documentElement.style
+            .setProperty("--repeat-at", -1 * 100 / copyAmount + "%");
+        
+        // Give each image about 12 seconds on the screen.
+        // Gives a relative speed so that no matter how many images are put in the slides folder,
+        // it'll stay a somewhat constant speed.
+        document.documentElement.style
+            .setProperty("--bannerSpeed", 12 * imageAmount + "s");
     }
     else
     {
-        $(".photobanner").append("<p>There are no images in the images/Slides folder!<br>Please add some.</p>");
+        $(".photobanner").append("<p>There are no images in the Slides folder!<br>Please add some.</p>");
     }
     
 });
